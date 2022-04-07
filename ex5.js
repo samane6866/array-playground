96f6341366c026bf3da301239ac0926a0465f0ee
omiras <oscar.miras@gmail.com>
2022-04-07 08:29:27 +0200

    regexp

--- ex5.js
+++ ex5.js
@@ -7,5 +7,6 @@ const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present',
 console.log(words.filter()); // TODO [ 'elite', 'exuberant']
 
 // Usa el método filter para filtrar las palabras que empiezan por vocal
+// BONUS: Usa una expresión regular!
 
 console.log(words.filter()); // TODO [ 'elite', 'exuberant', 'away']
